7b9bb44f784d7d42aa1cf47cbdcdafb9abc55b79
lanius <lanius@foxmail.com>
2022-10-18 00:07:18 +0800

    完成登录模块基础布局

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,41 @@
-import { Button } from 'antd'
+import { Card } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import logo from '@/assets/react.svg'
+//导入样式
+import './index.scss'
+
 const Login = () => {
   return (
-    <div className="login-content">
-      <div>login</div>
-      <Button type="primary">Button</Button>
+    <div className="login">
+      <Card className='login-container'>
+        <img className='login-logo' src={logo} />
+        {/* 登录表单 */}
+        <Form
+          initialValues={{
+            remember: true,
+          }}
+        >
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+          >
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
