2585fa8c32e400622771383b8ecba6190adaffb7
lanius <lanius@foxmail.com>
2022-10-20 18:45:59 +0800

    完成登录表单校验 axios统一封装

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,22 +5,47 @@ import logo from '@/assets/react.svg'
 import './index.scss'
 
 const Login = () => {
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <div className="login">
       <Card className='login-container'>
         <img className='login-logo' src={logo} />
         {/* 登录表单 */}
         <Form
+          validateTrigger={['onBlur', 'onChange']}
           initialValues={{
             remember: true,
           }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Form.Item>
+          {/* 用户名区域 */}
+          <Form.Item
+            name="username"
+            rules={[
+              { required: true, message: '请输入手机号' },
+              { pattern: /^1[3-9]\d{9}$/, message: '请输入正确的手机号', validateTrigger: 'onBlur' }]}
+          >
             <Input size="large" placeholder="请输入手机号" />
           </Form.Item>
-          <Form.Item>
+
+          {/* 密码区域 */}
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '请输入验证码' },
+            { len: 6, message: '请输入正确的验证码', validateTrigger: 'onBlur' }]}
+          >
             <Input size="large" placeholder="请输入验证码" />
           </Form.Item>
+
           <Form.Item
             name="remember"
             valuePropName="checked"
---
+++ src/store/index.jsx
@@ -0,0 +1,16 @@
+// 把所有的模块做统一处理
+// 导出一个统一的方法useStore
+import LoginStore from './login.Store'
+import React from 'react';
+class RootStore{
+  constructor(){
+    this.loginStore=new LoginStore()
+  }
+
+}
+
+// 实例化操作
+const rootStore=new RootStore()
+const context=React.createContext(rootStore)
+const useStore=()=>React.useContext(context)
+export {useStore}
