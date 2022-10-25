4f9a5738d286f160464f948a48b494f53f58a799
lanius <lanius@foxmail.com>
2022-10-25 23:16:43 +0800

    完成layout二级路由和导航点击跳转

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import './App.css';
 // 导入页面组件
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
+import Home from '@/pages/Home'
+import Article from '@/pages/Article';
+import Publish from '@/pages/Publish'
 
 import { AuthComponent } from '@/components/AuthComponent';
 
@@ -13,15 +16,20 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-          <Routes>
-            {/* Layout需要鉴权处理的 */}
-            <Route path="/*" element={
-              <AuthComponent>
-                <Layout />
-              </AuthComponent>} ></Route>
-            {/* 这个不需要 */}
-            <Route path="/login" element={<Login />} />
-          </Routes>
+        <Routes>
+          {/* Layout需要鉴权处理的 */}
+          <Route path="/*" element={
+            <AuthComponent>
+              <Layout />
+            </AuthComponent>} >
+              {/* 嵌套子路由 */}
+            <Route index element={<Home />}></Route>
+            <Route path='publish' element={<Publish />}></Route>
+            <Route path='article' element={<Article />}></Route>
+          </Route>
+          {/* 这个不需要 */}
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </div>
   )
--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -6,19 +6,34 @@ import {
   LogoutOutlined
 } from '@ant-design/icons'
 import './index.scss'
+import {Outlet, useNavigate,useLocation} from 'react-router-dom';
 
 const { Header, Sider } = Layout
 
 const GeekLayout = () => {
 
   //menu items
+  // const items=[
+  //   { label: <Link to='/'>数据概览</Link>, key: '1' }, // 菜单项务必填写 key
+  //   { label: <Link to="/article">内容管理</Link>, key: '2' },
+  //   { label: <Link to="/publish">发布文章</Link>, key: '3' },
+  // ];
+
   const items=[
-    { label: '数据概览', key: '1' }, // 菜单项务必填写 key
-    { label: '内容管理', key: '2' },
-    { label: '发布文章', key: '3' },
-    
+    { label: '数据概览', key: '/',icon:<HomeOutlined/> }, // 菜单项务必填写 key
+    { label: '内容管理', key: '/article',icon:<DiffOutlined/> },
+    { label: '发布文章', key: '/publish',icon:<EditOutlined/> },
   ];
 
+  // 导航点击跳转事件
+  const navigator=useNavigate()
+  const clickMenu=(e)=>{
+    // console.log(e);
+    const {key}=e
+    navigator(key)
+  }
+
+  const {pathname}=useLocation() // 默认是: /
   return (
     <Layout>
       <Header className="header">
@@ -37,13 +52,18 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            defaultSelectedKeys={[pathname]}
+            selectedKeys={[pathname]}
             style={{ height: '100%', borderRight: 0 }}
             items={items}
+            onClick={(e)=>clickMenu(e)}
           >
           </Menu>
         </Sider>
-        <Layout className="layout-content" style={{ padding: 20 }}>内容</Layout>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          {/* 二级路由 */}
+        <Outlet></Outlet>
+        </Layout>
       </Layout>
     </Layout>
   )
